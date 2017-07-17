4ba04c4b1d573a44c6ba2f92a024dc66b159ef05
onehao <onehao333@gmail.com>
2017-07-17 15:30:25 +0800

    add testng & junit test and update ignore files.

---
+++ src/queue/test/BasicTest.java
@@ -0,0 +1,14 @@
+package queue.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BasicTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/queue/test/NewTest.java
@@ -0,0 +1,9 @@
+package queue.test;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
