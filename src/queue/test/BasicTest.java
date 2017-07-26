72ebe566e6f9cece3bd5398cd88b2f017bebffdb
onehao <onehao333@gmail.com>
2017-07-26 15:49:59 +0800

    update tests.

--- src/queue/test/BasicTest.java
+++ src/queue/test/BasicTest.java
@@ -8,7 +8,21 @@ public class BasicTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Integer int1 = Integer.valueOf("100");
+        Integer int2 = Integer.valueOf("100");
+        System.out.println(int1 == int2);
+        Integer int3 = Integer.valueOf("200");
+        Integer int4 = Integer.valueOf("200");
+        System.out.println(int3 == int4);
 	}
+	
+	public static void main(String[] args) {
+        Integer int1 = Integer.valueOf("100");
+        Integer int2 = Integer.valueOf("100");
+        System.out.println(int1 == int2);
+        Integer int3 = Integer.valueOf("200");
+        Integer int4 = Integer.valueOf("200");
+        System.out.println(int3 == int4);
+    }
 
 }
